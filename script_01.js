c2351215e3e2690998f03fc6f9de670ef2a42d75
Peter <schmidt.multimedia@gmail.com>
2022-07-11 09:16:22 +0200

    02 / IF - ELSE

--- script_01.js
+++ script_01.js
@@ -27,13 +27,27 @@ console.log("------------------");
 // if (true)
 // if (false)
 // if (isJohnOlder)
-if (ageJohn > ageMark)
-{
-    console.log("John ist älter!");
-}
+// if (ageJohn > ageMark)
+// {
+//     console.log("John ist älter!");
+// }
 
 /************ IF - ELSE  ************/
 // mit Alternative  
 // entweder ja oder nein
 
+if (isJohnOlder) 
+{
+    console.log("John ist älter!");
+} 
+else 
+{
+    console.log("John ist jünger!");
+}
+
+/************  Ternäre (ternary) Schreibweise ************/
+// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+/************ IF - ELSE IF  ************/
+// mit alternativen Fällen (älter, jünger, gleich alt)
 
