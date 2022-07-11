ce83eb6d80700f09c23a8bce8349af02821119a0
Peter <schmidt.multimedia@gmail.com>
2022-07-11 08:59:34 +0200

    01 / if

--- script_01.js
+++ script_01.js
@@ -2,3 +2,38 @@
 
 // Entscheidungsstrukturen | control flow
 
+// Deklaration + Assignment
+const ageJohn = 30;
+const ageMark = 30;
+
+// Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+
+// if (true)
+// if (false)
+// if (isJohnOlder)
+if (ageJohn > ageMark)
+{
+    console.log("John ist älter!");
+}
+
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+
