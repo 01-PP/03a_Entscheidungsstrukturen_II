f893434e7ed80367ef5da4ff6abe2de0ca9b4b68
Peter <schmidt.multimedia@gmail.com>
2022-07-11 10:21:30 +0200

    04 / switch / case 1

--- script_01.js
+++ script_01.js
@@ -14,11 +14,11 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
 
 /************ IF  ************/
 // TINA --> There is no alternative!
@@ -51,20 +51,50 @@ console.log("------------------");
 /************ IF - ELSE IF  ************/
 // mit alternativen Fällen (älter, jünger, gleich alt)
 
-if (isJohnOlder) // 1. Fall
-{
-    console.log("John ist älter!");
-} 
-else if (isJohnEqual) // 2. Fall
-{
-    console.log("John ist gleich alt!");
-} 
-// evtl. weitere Alternative(n) else if(){}
-else // gemeinsamer else-zweig
-{
-    console.log("John ist jünger!");
-}
+// if (isJohnOlder) // 1. Fall
+// {
+//     console.log("John ist älter!");
+// } 
+// else if (isJohnEqual) // 2. Fall
+// {
+//     console.log("John ist gleich alt!");
+// } 
+// // evtl. weitere Alternative(n) else if(){}
+// else // gemeinsamer else-zweig
+// {
+//     console.log("John ist jünger!");
+// }
 
 /****** Fallunterscheidung / SWITCH|CASE 1 ******/
 
+const firstName = "Jane";
+let job;
 
+job = "driver";     // .. fährt TAXI! / UBER
+// job = "diver";      // .. taucht im Rhein! 
+// job = "artist";     // .. malt ein Bild!
+// job = "pilot";      // .. macht etwas anderes! --> default
+// job = "teacher";    // .. unterrichtet!
+// job = "instructor";  // .. unterrichtet!
+
+switch (job) 
+{
+    case "driver": // Test auf ==
+        console.log(firstName + " fährt TAXI!");
+        break;
+    case "diver": // Test auf ==
+        console.log(firstName + " taucht im Rhein!");
+        break;
+    case "artist": // Test auf ==
+        console.log(firstName + " malt ein Bild!");
+        break;
+    
+    case "teacher": // Test auf ==
+    case "instructor": // Test auf ==
+        console.log(firstName + " unterrichtet!");
+        break;
+
+    default: // Wichtig!
+        console.log(firstName + " macht etwas anderes!");
+        break;
+}
